9c0476016140e772c3a6a574e4154d0e7d07f803
Delia Hamwood <dhamwood@MB-B9419.local>
2021-10-21 10:48:09 +0100

    Added robotjs for keyboard control to be able to type card id

--- uid-read-14443-4.js
+++ uid-read-14443-4.js
@@ -1,15 +1,22 @@
+
 const NFC = require('nfc-pcsc').NFC;
+//const ks = require('node-key-sender');
 const nfc = new NFC();
 console.log( 'Started...' );
 
+var lastID;
+const robot = require("robotjs");
+
+
 nfc.on( 'reader', function( reader ) {
 	reader.autoProcessing = false;
+
 
 	console.log( reader.reader.name + ' device attached' );
 
 	reader.on( 'card', function ( card ) {
 		console.log( card )
-		
+
 		const packet = new Buffer([ 0xff, 0xca, 0x00, 0x00, 0x00 ]);
 		const response = reader.transmit(packet, 12).then( function( data ) {
 			if (data.length < 2) {
@@ -24,6 +31,7 @@ nfc.on( 'reader', function( reader ) {
 			const uid = data.slice(0, -2).toString('hex');
 			newTag( uid.toUpperCase() );
 		});
+
 	} );
 
 	reader.on( 'error', function ( err ) {
@@ -41,4 +49,11 @@ nfc.on( 'error', function ( err ) {
 
 function newTag( uid ) {
 	console.log( 'New tag: ' + uid );
+	// Type "Hello World" then press enter.
+
+// Type "Hello World".
+ robot.typeString(uid);
+
+ // Press enter.
+	robot.keyTap("enter");
 }
